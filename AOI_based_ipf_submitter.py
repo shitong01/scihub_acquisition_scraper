7518cc3f090b823ff4bdf933129643aed7b746e8
jlinick <Justin.P.Linick@jpl.nasa.gov>
2019-04-01 18:12:51 +0000

    malarout: enabling dedup of IPF scraper jobs

--- AOI_based_ipf_submitter.py
+++ AOI_based_ipf_submitter.py
@@ -114,8 +114,7 @@ def submit_ipf_scraper(acq):
     print(json.dumps(params, sort_keys=True, indent=4, separators=(',', ': ')))
     mozart_job_id = submit_mozart_job({}, rule, hysdsio={"id": "internal-temporary-wiring", "params": params,
                                                          "job-specification": "job-ipf_scraper:master"},
-                                      job_name='job-%s-%s-%s' % ("ipf_scraper", acq.get("id"), "master"),
-                                      enable_dedup=False)
+                                      job_name='job-%s-%s-%s' % ("ipf_scraper", acq.get("id"), "master"))
     print("For {} , IPF scrapper Job ID: {}".format(acq.get("id"), mozart_job_id))
 
 
